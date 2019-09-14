b5cbe929b9edc5b301aaf193e4c06240aa3bdad4
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-14 14:57:03 -0400

    fixed extras typo in db_interface

--- api/db_interface.py
+++ api/db_interface.py
@@ -15,7 +15,7 @@ def add(urls: List[str], source: str, extras=None) -> List[int]:
         ident = rdb.incr('idcount')
         rdb.set(f'recipe:{ident}:url', url)
         rdb.set(f'recipe:{ident}:source', source)
-        if extra != None:
+        if extras != None:
             for extra in extras:
                 rdb.set(f'recipe:{ident}:{extra}', extras[extra][count])
         idents.append(ident)
