985829cb8b94d5446cb36fad4c10c047860c003e
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-14 13:09:28 -0400

    Dont blindly index dict

--- api/sources/allrecipes_api.py
+++ api/sources/allrecipes_api.py
@@ -17,8 +17,8 @@ def search(ingredients: List[str]) -> List[int]:
 
 def lookup(url: str) -> Recipe:
     recipe_details = ar.get(url)
-    name = recipe_details['name']
-    ingredients = recipe_details['ingredients']
-    time = recipe_details['total_time']
-    rating = recipe_details['rating'] 
+    name = recipe_details.get('name')
+    ingredients = recipe_details.get('ingredients')
+    time = recipe_details.get('total_time']
+    rating = recipe_details.get('rating')
     return Recipe(name, ingredients, time, rating)
