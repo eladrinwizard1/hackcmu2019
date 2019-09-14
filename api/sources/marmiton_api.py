009c84f34dde85a86c257e6d2ccc3a8a8c3b2e9d
Daniel Zeng <danielzeng@Daniels-MBP.wv.cc.cmu.edu>
2019-09-14 02:58:21 -0400

    made marmiton but it's broken

---
+++ api/sources/marmiton_api.py
@@ -0,0 +1,11 @@
+#from marmiton import Marmiton
+#import db_interface
+#from typing import List
+
+#def search(ingredients: List[str]) -> List[int]:
+    #query_options = {
+        'aqt' : ' '.join(ingredients),
+    }
+    #query_results = Marmiton.search(query_options)
+    #print(query_results)
+    #return db_interface.add([recipe['url'] for recipe in query_results])
