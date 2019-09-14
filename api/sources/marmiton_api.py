78dfe5e381785bd33bb7740095c5cad7eb114c9b
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-14 13:44:51 -0400

    Marmiton lookup fix

--- api/sources/marmiton_api.py
+++ api/sources/marmiton_api.py
@@ -1,4 +1,4 @@
-from marmiton import Marmiton
+from marmiton import Marmiton as mr
 from api import db_interface
 from api.structs.recipe import Recipe
 from typing import List
@@ -7,11 +7,11 @@ def search(ingredients: List[str]) -> List[int]:
     query_options = {
         'aqt' : ' '.join(ingredients),
     }
-    query_results = Marmiton.search(query_options)
+    query_results = mr.search(query_options)
     return db_interface.add([recipe['url'] for recipe in query_results], 'marmiton')
 
 def lookup(url: str) -> Recipe:
-    recipe_details = ar.get(url)
+    recipe_details = mr.get(url)
     name = recipe_details.get('name')
     ingredients = recipe_details.get('ingredients')
     time = recipe_details.get('total_time')
