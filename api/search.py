1eb837fe2c6452cce3de2be15f5151c1dd87c04d
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-14 04:42:48 -0400

    Basic search (broken)

--- api/search.py
+++ api/search.py
@@ -1,6 +1,11 @@
 from api import app
 from api.sources import allrecipes_api as ar_api
+from flask import jsonify
+from flask import request
 
 @app.route('/search', methods=['POST'])
 def search():
-    pass
+    ingredients = request.json['ingredients']
+    ids = []
+    ids.extend(ar_api.search(ingredients))
+    return jsonify(ids)
--- driver.py
+++ driver.py
@@ -1,1 +1,4 @@
 from api import app
+import api.db_interface
+
+api.db_interface.init_conn()
