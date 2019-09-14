05f5c44a86e0f8f467deba1fb065d26b2a38de26
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-13 23:07:18 -0400

    Fixed stupidity

--- api/sources/allrecipes.py
+++
@@ -1,7 +0,0 @@
-from allrecipes import AllRecipes as ar
-
-def search(ingredients: List[str]) -> List[dict]:
-    query_options = {
-        "ingIncl" : " ".join(ingredients),
-    }
-    print(ar.search(query_options))
