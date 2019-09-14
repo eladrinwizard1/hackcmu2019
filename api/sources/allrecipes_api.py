069bfdd2fad3e28fcdd73ef09145dbae20728d72
Cooper Pierce <cppierce@andrew.cmu.edu>
2019-09-14 03:34:23 -0400

    ar lookup recipe

--- api/sources/allrecipes_api.py
+++ api/sources/allrecipes_api.py
@@ -1,4 +1,5 @@
 from allrecipes import AllRecipes as ar
+from models import Recipe
 import db_interface
 from typing import List
 
@@ -10,5 +11,6 @@ def search(ingredients: List[str]) -> List[int]:
     query_results = ar.search(query_options)
     return db_interface.add([recipe['url'] for recipe in query_results])
 
-def lookup(ident: int) -> Recipe:
-    pass
+def lookup(url: str) -> Recipe:
+    recipe_details = ar.get(url)
+    return Recipe(recipe_details)
